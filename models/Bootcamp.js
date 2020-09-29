5484c7ee43ad259c7c647402248a0afe17b8b734
SamKlep <shamdemann23@gmail.com>
2020-09-29 16:00:08 -0500

    Create bootcamp functionalit implemendted.

--- models/Bootcamp.js
+++ models/Bootcamp.js
@@ -4,15 +4,15 @@ const BootcampSchema = new mongoose.Schema({
   name: {
     type: String,
     required: [true, 'Please add a name'],
-    uniquie: true,
+    unique: true,
     trim: true,
-    maxlength: [50, 'Name cannot be more than 50 characters'],
+    maxlength: [50, 'Name can not be more than 50 characters'],
   },
   slug: String,
-  decsription: {
+  description: {
     type: String,
     required: [true, 'Please add a description'],
-    maxlength: [500, 'Name cannot be more than 500 characters'],
+    maxlength: [500, 'Description can not be more than 500 characters'],
   },
   website: {
     type: String,
